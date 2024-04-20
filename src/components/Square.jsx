1db3eec1e8e6d83131f10d26718b1a6f7f8f4445
Dhanraj_Kamble <dhanrajkamble3518@gmail.com>
2024-04-20 15:16:28 +0530

    completed game

---
+++ src/components/Square.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Square = (props) => {
+    return (
+        <div
+            onClick={props.onClick}
+            className="square"
+            >
+            <h2>{props.value}</h2>
+        </div>
+    );
+}
+
+export default Square;
